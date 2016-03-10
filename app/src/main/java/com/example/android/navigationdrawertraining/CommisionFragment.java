6050659607824643d175eddb196e3ae401df7d8b
Manohar <kvmanohar@gmail.com>
2016-03-10 22:45:29 +0000

    - Navigation Menu with Listener and Fragment View added.

---
+++ app/src/main/java/com/example/android/navigationdrawertraining/CommisionFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.navigationdrawertraining;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CommisionFragment extends Fragment {
+
+
+    public CommisionFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_commision, container, false);
+    }
+
+}
--- app/src/main/java/com/example/android/navigationdrawertraining/MainActivity.java
+++ app/src/main/java/com/example/android/navigationdrawertraining/MainActivity.java
@@ -1,30 +1,46 @@
 package com.example.android.navigationdrawertraining;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+
+
+    android.support.v4.app.FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Add toolbar to the actionbar in the application
+        //Initialise toolbar to application action bar//
         toolbar = (Toolbar) findViewById(R.id.toolbar_actionBar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Manohar");
+        assert getSupportActionBar() != null;
 
-         //Add drawerLayout
-        drawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
+        //Initialise drawerLayout and add listener//
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close);
+
+        //Initialise FragmentTransaction and set the first page to Policy page//
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container, new PolicyFragment());
+        fragmentTransaction.commit();
+        getSupportActionBar().setTitle(R.string.policy_fragment);
+
+        //Set the Listener to NavigationView//
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(navigationViewListener);
 
     }
 
@@ -33,4 +49,43 @@ public class MainActivity extends AppCompatActivity {
         super.onPostCreate(savedInstanceState);
         actionBarDrawerToggle.syncState();
     }
+
+    public NavigationView.OnNavigationItemSelectedListener navigationViewListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+
+            assert getSupportActionBar() != null;
+            switch (item.getItemId()) {
+                case R.id.nav_policy:
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.main_container, new PolicyFragment());
+                    fragmentTransaction.commit();
+                    getSupportActionBar().setTitle(R.string.policy_fragment);
+                    item.setChecked(true);
+                    drawerLayout.closeDrawers();
+                    break;
+
+                case R.id.nav_commission:
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.main_container, new CommisionFragment());
+                    fragmentTransaction.commit();
+                    getSupportActionBar().setTitle(R.string.commission_fragment);
+                    item.setChecked(true);
+                    drawerLayout.closeDrawers();
+                    break;
+
+                case R.id.nav_setting:
+                    fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.main_container, new SettingsFragment());
+                    fragmentTransaction.commit();
+                    getSupportActionBar().setTitle(R.string.settings_fragment);
+                    item.setChecked(true);
+                    drawerLayout.closeDrawers();
+                    break;
+            }
+            return false;
+        }
+    };
+
+
 }
