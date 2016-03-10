018078329bd1dd58fe64e13a02a4a41054e3f327
Manohar <kvmanohar@gmail.com>
2016-03-10 00:35:34 +0000

    - Updated the title of the support toolbar

--- app/src/main/java/com/example/android/navigationdrawertraining/MainActivity.java
+++ app/src/main/java/com/example/android/navigationdrawertraining/MainActivity.java
@@ -1,31 +1,30 @@
 package com.example.android.navigationdrawertraining;
 
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-//    Toolbar toolbar;
+    Toolbar toolbar;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
-ActionBar actionBar;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        drawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
-        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
-        drawerLayout.setDrawerListener(actionBarDrawerToggle);
-
         //Add toolbar to the actionbar in the application
-//        toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Manohar");
+
+         //Add drawerLayout
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
 
     }
 
